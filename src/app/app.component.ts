2c9aa664b48bed3dd75e158bb412537a861b446c
Diego de la jara <diegodelajara@MacBook-Pro-de-Diego.local>
2024-12-18 14:19:36 -0300

    :tada:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todoapp';
+  tasks =
+    [
+      "Revisión de correos y mensajes pendientes",
+      "Planificación del día",
+      "Trabajo en tareas clave (Deep Work)",
+      "Pausa corta",
+      "Reuniones o colaboraciones",
+      "Tareas rápidas o pendientes menores",
+      "Pausa para almuerzo",
+      "Desarrollo o aprendizaje",
+      "Revisión y ajustes finales",
+      "Planificación del próximo día"
+    ];
 }
